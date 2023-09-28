6d2dc78fbf4755d643b9839218c18f275400562e
Michaela Vlckova <vlckova.michaela5@gmail.com>
2023-09-28 20:37:03 +0200

    commit inspector coding

--- src/webview/render/components/CommitInspector/index.tsx
+++ src/webview/render/components/CommitInspector/index.tsx
@@ -1,4 +1,4 @@
-import { GitCommitFileMode } from "../../../../types/git.js";
+import { GitFileMode } from "../../../../types/git.js";
 import { GraphNode } from "../../../state/createGraphNodes/index.js";
 import { useWebviewStore } from "../../../state/index.js";
 import { renderEmptyRails } from "../GraphRow/renderRails.js";
@@ -12,14 +12,21 @@ export const CommitInspector = ({ node }: { node: GraphNode }) => {
 		<tr class="commit-inspector">
 			<td>{renderEmptyRails(node, 200)}</td>
 			<td colSpan={4}>
-				<div class="flex leading-normal h-[200px] overflow-scroll">
-					<div>
-						<div>{commit.hash}</div>
+				<div class="flex leading-normal h-[200px] overflow-auto w-full">
+					<div class="w-1/2 p-1 overflow-hidden text-ellipsis">
+						<div>
+							<span class="font-bold">Commit: </span>
+							<span>{commit.hash}</span>
+						</div>
+						<div>
+							<span class="font-bold">Author: </span>
+							<span>{commit.author}</span>
+						</div>
+						<br />
 						<div>{commit.subject}</div>
-						<div>{commit.author}</div>
 					</div>
-					<div>
-						<ul class="list-none">
+					<div class="w-1/2 p-1 overflow-y-auto overflow-x-hidden text-ellipsis">
+						<ul class="files list-none">
 							{commit.files.map((f, i) => (
 								<li
 									key={f.path}
@@ -33,8 +40,10 @@ export const CommitInspector = ({ node }: { node: GraphNode }) => {
 									}
 									class={`flex gap-2 p-1 ${fileModeColors[f.mode] ?? "white"}`}
 								>
-									<span>{f.mode}</span>
-									<span>{f.path}</span>
+									<div class="flex justify-between w-full">
+										<span>{f.path}</span>
+										<span class="px-1">{f.mode}</span>
+									</div>
 								</li>
 							))}
 						</ul>
@@ -45,10 +54,14 @@ export const CommitInspector = ({ node }: { node: GraphNode }) => {
 	);
 };
 
-const fileModeColors: Record<GitCommitFileMode, string> = {
-	A: "text-green-800",
-	D: "text-rose-700",
-	M: "text-yellow-600",
-	R: "text-lime-800",
-	U: "text-stone-500",
+const fileModeColors: Record<GitFileMode, string> = {
+	M: "modified",
+	T: "type-modified",
+	A: "added",
+	D: "deleted",
+	R: "renamed",
+	C: "copied",
+	U: "updated",
+	"!": "ignored",
+	"?": "untracked",
 };
