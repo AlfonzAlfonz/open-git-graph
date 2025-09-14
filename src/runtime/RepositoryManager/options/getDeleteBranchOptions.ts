829fad977ef2444d67a63be5be09bc8825163eae
Denis Homolík <alfonz@homolik.cz>
2025-09-15 00:31:42 +0200

    remove remotes flag
    replace remotes flag with running git push -d

--- src/runtime/RepositoryManager/options/getDeleteBranchOptions.ts
+++ src/runtime/RepositoryManager/options/getDeleteBranchOptions.ts
@@ -1,15 +1,17 @@
+import * as vscode from "vscode";
 import { showOptionPicker } from "../../showOptionPicker";
 import { DeleteBranchOptions } from "../git/commands/gitBranchDelete";
 
-export const getDeleteBranchOptions = async (): Promise<
-	(DeleteBranchOptions & { remotes: boolean }) | undefined
-> => {
+export const getDeleteBranchOptions = async (
+	branch: string,
+): Promise<(DeleteBranchOptions & { remotes: boolean }) | undefined> => {
 	const selected = await showOptionPicker({
 		items: [
 			{
 				label: "--force",
 				description: "Delete branch even if it can lead to lost commits",
 			},
+			{ label: "Other options", kind: vscode.QuickPickItemKind.Separator },
 			{
 				label: "Remove tracking branches",
 				type: "other",
@@ -18,7 +20,10 @@ export const getDeleteBranchOptions = async (): Promise<
 		] as const,
 		getTitle: () => "Execute command",
 		getPlaceholder: (items) =>
-			`git cherry-pick ${items.map((i) => i.label).join(" ")}`,
+			`git branch -d ${items
+				.filter((i) => i.type !== "other")
+				.map((i) => i.label)
+				.join(" ")} ${branch}`,
 	});
 
 	if (selected === undefined) return;
